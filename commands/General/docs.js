3bc2d3f8fa21e3c13fed218d73f11193c41e4f00
芭无寓 度麻素 <bangdompret@gmail.com>
2020-10-03 08:02:19 +0700

    Create docs.js

---
+++ commands/General/docs.js
@@ -0,0 +1,35 @@
+const fetch = require('node-fetch')
+module.exports={
+    name: "docs",
+    description: "Get  docs from documentation discord.js",
+    usage: "docs",
+    enabled: true,
+    guildOnly: true,
+    aliases: [],
+    memberPermissions: [],
+    botPermissions: ["SEND_MESSAGES"],
+    nsfw: false,
+    cooldown: 5000,
+    ownerOnly: false,
+
+   async execute (client, message, args){
+       const searchQuery = args.join(" ");
+       const url = `https://djsdocs.sorta.moe/v2/embed?src=stable&q=${encodeURIComponent(searchQuery)}`;
+
+       fetch(url)
+       .then((res)=> res.json())
+       .then((embed)=>{
+           if(embed && !embed.error){
+               message.channel.send({embed})
+           }else { 
+               message.reply(`
+               I don't know about you but ${searchQuery} isn't a valid doc.
+               `)
+           };
+       })
+       .catch((e)=>{
+           message.reply('Woops, there\'s been an error. Check console for details.');
+       });
+    
+    },
+};
