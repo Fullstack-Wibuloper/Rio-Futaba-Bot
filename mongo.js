5f5727c41665b5097249f2c7653b4395c121ff9b
バング ドマス <bangdompret@gmail.com>
2021-10-01 05:28:57 +0700

    Create mongo.js

---
+++ mongo.js
@@ -0,0 +1,8 @@
+const MongoClient = require('mongodb').MongoClient;
+const uri = "url";
+const client = new MongoClient(uri, { useNewUrlParser: true });
+client.connect(err => {
+  const collection = client.db("test").collection("devices");
+  // perform actions on the collection object
+  client.close();
+});
